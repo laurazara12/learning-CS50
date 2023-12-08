348295a0b27be31da3d4b9f171fc1899962f5b2f
Laura Zara <115985878+laurazara12@users.noreply.github.com>
2023-12-08 03:44:31 -0800

    Delete learning-sql/CS50SQL/views directory

--- learning-sql/CS50SQL/views/9.sql
+++
@@ -1,5 +0,0 @@
-/*In 9.sql, write a SQL query to find the English title and artist of the print with the highest brightness.*/
-SELECT english_title, artist
-FROM views
-ORDER BY brightness DESC
-LIMIT 1;
